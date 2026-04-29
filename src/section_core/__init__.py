3afad2b4796b82f3db0736bed5d3d74786e05972
Bruno Coloccini <bruno.coloccini@eeii.com.ar>
2026-04-29 14:51:24 -0300

    Implement section geometry primitives and tolerances

--- src/section_core/__init__.py
+++ src/section_core/__init__.py
@@ -1,5 +1,6 @@
 """section_core package."""
 
+from .geometry import GeometryTolerance, SectionLine, SectionPoint
 from .units.quantity import Quantity
 
-__all__ = ["Quantity"]
+__all__ = ["GeometryTolerance", "Quantity", "SectionLine", "SectionPoint"]
